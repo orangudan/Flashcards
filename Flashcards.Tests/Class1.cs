e6ac64bb8fa2f4f25be0fd0e1f84c371f39a0f04
Daniel Harrison <harrison92d+orangudan@gmail.com>
2017-07-20 21:33:47 +0100

    Initial solution with NUnit test project

---
+++ Flashcards.Tests/Class1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Flashcards.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.Fail("Testing the testing framework!");
+        }
+    }
+}
