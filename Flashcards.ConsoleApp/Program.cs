a6158543a4f40364b9d12940b70eae9c4f49c8c0
Daniel Harrison <harrison92d+orangudan@gmail.com>
2017-07-20 22:26:13 +0100

    Added console app project

---
+++ Flashcards.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Flashcards.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Flashcards");
+        }
+    }
+}
